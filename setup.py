ac324b58f7db8f7593f6cc27c4f95ebc50853718
Francisco Griman <grihardware@gmail.com>
2024-09-21 16:14:01 -0400

    Update version to 0.0.2 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name='flask-server-status',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/fcoagz/flask-status/',
     license='MIT',
     author='Francisco Griman',
