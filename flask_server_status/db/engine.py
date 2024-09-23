b2cf0bf1dbe8bc2828d53533b7ac92e6fac68f14
Francisco Griman <grihardware@gmail.com>
2024-09-22 22:18:15 -0400

    Use cache for get URI

--- flask_server_status/db/engine.py
+++ flask_server_status/db/engine.py
@@ -1,16 +1,9 @@
 import os
+import re
 from sqlalchemy import create_engine
-from .models import Base
+from ..utils.cache import cache
 
-def get_engine(uri: str) -> create_engine:
-    """
-    Create engine for database connection
-    """
+if re.findall('tmp', cache['SQLALCHEMY_DATABASE_URI']):
     os.makedirs('tmp', exist_ok=True)
-    return create_engine(uri)
 
-def create_tables(engine) -> None:
-    """
-    Create tables in database
-    """
-    Base.metadata.create_all(engine)
+engine = create_engine(cache['SQLALCHEMY_DATABASE_URI'])
